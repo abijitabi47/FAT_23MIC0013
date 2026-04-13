d8290a28da97f02984139a91cadf3656aee1a8b0
abijitabi47 <abijitib47thrissur@gmail.com>
2026-04-13 16:10:23 +0530

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Student Name: Abijit I B");
+		System.out.println("Register number : 23MIC0013");
+		System.out.println("Course Name : Application Development and Deployment Architecture");
+		System.out.println("Department : Computer Science");
+		System.out.println("Attendance percentage : 85%");
+		System.out.println("Number of Classes Conducted : 100");
+		System.out.println("Number of Classes Attended : 85");
+		System.out.println("Attendance Build Successful");
+	}
+}
